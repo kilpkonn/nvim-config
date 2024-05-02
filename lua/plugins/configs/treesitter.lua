54ae9ef0f3b93afffc5d5b8f7c41a8901b18586d
Tavo Annus <tavo.annus@gmail.com>
2024-05-02 10:22:10 +0300

    Add highlights for nu shell

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -13,6 +13,14 @@ parser_config.matlab = {
     filetype = "matlab",
 }
 
+parser_config.nu = {
+  install_info = {
+    url = "https://github.com/nushell/tree-sitter-nu",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+  filetype = "nu",
+}
 
 ts_config.setup {
     ensure_installed = {
