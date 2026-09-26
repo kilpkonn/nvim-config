390e8a249f17827f0c4cd9db90f71797f47ddc89
Tavo Annus <tavo.annus@gmail.com>
2026-09-26 10:20:20 +0300

    Swap autopairs plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,11 +1,13 @@
 vim.pack.add({
-  { src = 'https://github.com/altermo/ultimate-autopair.nvim' },
+  { src = 'https://github.com/windwp/nvim-autopairs' },
   { src = 'https://github.com/lukas-reineke/indent-blankline.nvim' },
   { src = 'https://github.com/wakatime/vim-wakatime' },
   { src = 'https://github.com/tpope/vim-fugitive' },
   { src = 'https://github.com/tpope/vim-dadbod' },
   { src = 'https://github.com/folke/trouble.nvim' },
 })
+
+require('nvim-autopairs').setup({})
 
 require("ibl").setup({
     scope = { show_start = false, show_end = false, },
